8399d72f06eae53e7353f1e40876deed9e305858
SaGerz <genarosamuel24@gmail.com>
2025-07-19 08:35:50 +0700

    chore: Add Configuration body-pareser and cores

--- API/index.js
+++ API/index.js
@@ -8,9 +8,14 @@ const taskManagementAccessRoutes = require('./routes/taskManagementAccessRoutes.
 const workingHistoryRoutes = require('./routes/workingHistoryRoutes.js');
 const verifyToken = require('./middleware/verifyToken.js');
 const authorize = require('./middleware/authorize.js');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-app.use(express.json());
-app.use('/api/auth', authRoutes);
+// app.use(express.json());
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use('/api/auth', authRoutes);   
 app.use('/api/absensi',verifyToken, absensiRoutes);
 app.use('/api/task-management', verifyToken, authorize([1]), taskManagementRoutes);
 app.use('/api/task-access', verifyToken, authorize([1, 2, 3]), taskManagementAccessRoutes);
